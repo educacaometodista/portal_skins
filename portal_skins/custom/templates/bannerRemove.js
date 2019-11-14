085b401bac87b15698ca6c7ffc44c9bc5859ebf7
LeuAlmeida <leo@webid.net.br>
2019-11-14 14:10:10 -0300

    Finalização da função para alteração do banner da Bolsa Social em 15/11

--- portal_skins/custom/templates/bannerRemove.js
+++ portal_skins/custom/templates/bannerRemove.js
@@ -14,16 +14,7 @@ const actualDate = new Date();
 const actualTimestamp = actualDate.getTime();
 
 // Data final (quando deverá ser removido)
-// const removeDate = new Date(2019, 11, 15, 03, 0, 0);
-// const removeTimestamp = removeDate.getTime()
+const removeDate = new Date(2019, 10, 15, 00, 0, 0);
+const removeTimestamp = removeDate.getTime()
 
-// if (actualTimestamp >= removeTimestamp) { bannerChange() }
-
-
-
-// Data final apenas para teste
-const removeTestDate = new Date(2019, 11, 14, 17, 0, 0);
-const removeTestTimestamp = removeTestDate.getTime()
-
-// Modelo de teste
-if (actualTimestamp >= removeTestTimestamp) { bannerRemove() }
+if (actualTimestamp >= removeTimestamp) { bannerChange() }
